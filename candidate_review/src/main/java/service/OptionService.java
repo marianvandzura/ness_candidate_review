a2f88bd17be7f5681eed8187301cfe91908a7f6a
LubomirLesko <lubomir.lesko.2@student.tuke.sk>
2015-11-04 20:17:01 +0100

    Oprava

--- candidate_review/src/main/java/service/OptionService.java
+++ candidate_review/src/main/java/service/OptionService.java
@@ -1,13 +1,6 @@
 package service;
 
-<<<<<<< HEAD
-import assemblers.OptionAssembler;
 import dao.IOptionsDao;
-import dao.IQuestionsDao;
-import dto.OptionDto;
-=======
-import dao.IOptionsDao;
->>>>>>> refs/remotes/origin/master
 import model.Options;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,33 +8,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 /**
-<<<<<<< HEAD
- * Created by Marian_Vandzura on 3.11.2015.
-=======
  * Created by Peter on 4.11.2015.
->>>>>>> refs/remotes/origin/master
  */
 @Service
 public class OptionService {
 
     @Autowired
-<<<<<<< HEAD
-    IOptionsDao optionsDao;
-
-    @Autowired
-    OptionAssembler optionAssembler;
-
-    @Autowired
-    IQuestionsDao questionsDao;
-
-    public Options addOption(final OptionDto optionDto){
-        Options option =  optionAssembler.populateDomainFromDto(optionDto, questionsDao);
-        return optionsDao.addOption(option);
-    }
-
-    public List<OptionDto> findOptionsByQuestionId (final Integer questionId) {
-        return optionAssembler.extractDtoListFromDomain(optionsDao.findOptionsByQuestion(questionId));
-=======
     private IOptionsDao optionsDao;
 
     public List<Options> findAll() {
@@ -58,6 +30,5 @@ public class OptionService {
 
     public List<Options> findByQuestion(final Integer questionId) {
         return optionsDao.findOptionsForQuestion(questionId);
->>>>>>> refs/remotes/origin/master
     }
 }
