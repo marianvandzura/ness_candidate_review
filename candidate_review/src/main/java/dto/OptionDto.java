9efa21884e3e7a3ab2f5e2c3d0e572e8838b831d
LubomirLesko <lubomir.lesko.2@student.tuke.sk>
2015-11-04 20:36:40 +0100

    new

--- candidate_review/src/main/java/dto/OptionDto.java
+++ candidate_review/src/main/java/dto/OptionDto.java
@@ -3,41 +3,22 @@ package dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
-<<<<<<< HEAD
- * Option Data Transfer Object
- * <p>
- * Created by Marian_Vandzura on 3.11.2015.
-=======
  * Created by Peter on 4.11.2015.
->>>>>>> refs/remotes/origin/master
  */
 public class OptionDto {
 
     @JsonProperty("id")
     private Integer id;
 
-<<<<<<< HEAD
-=======
     @JsonProperty("question")
     private QuestionDto question;
 
->>>>>>> refs/remotes/origin/master
     @JsonProperty("option")
     private String option;
 
     @JsonProperty("truth")
     private Boolean truth;
 
-<<<<<<< HEAD
-    @JsonProperty("questionId")
-    private Integer questionId;
-
-    public OptionDto() {
-        //default
-    }
-
-=======
->>>>>>> refs/remotes/origin/master
     public Integer getId() {
         return id;
     }
@@ -62,20 +43,11 @@ public class OptionDto {
         this.truth = truth;
     }
 
-<<<<<<< HEAD
-    public Integer getQuestionId() {
-        return questionId;
-    }
-
-    public void setQuestionId(Integer questionId) {
-        this.questionId = questionId;
-=======
     public QuestionDto getQuestion() {
         return question;
     }
 
     public void setQuestion(QuestionDto question) {
         this.question = question;
->>>>>>> refs/remotes/origin/master
     }
 }
