f871588d7160baedc125791dfb6e774cc4e151c1
Marian Vandzura <marian.vandzura@gmail.com>
2015-11-09 18:55:44 +0100

    pom update

--- candidate_review/src/main/java/controllers/CategoryController.java
+++ candidate_review/src/main/java/controllers/CategoryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import service.CategoryService;
 
@@ -16,7 +17,7 @@ import java.util.List;
  * <p>
  * Created by Marian_Vandzura on 7.11.2015.
  */
-@Component
+@Controller
 public class CategoryController {
 
     @Autowired
