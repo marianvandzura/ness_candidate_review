cd1b6add2e29cac04977d7961f9f3cd1ab8ca6a8
Marian Vandzura <marian.vandzura@gmail.com>
2015-11-20 20:30:28 +0100

    security-config pwd encoder

--- candidate_review/src/main/java/model/UserRole.java
+++ candidate_review/src/main/java/model/UserRole.java
@@ -10,6 +10,9 @@ import java.util.Collection;
 @Table(name = "user_roles")
 public class UserRole {
 
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final String ROLE_USER = "ROLE_USER";
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_role_id", unique = true, nullable = false)
