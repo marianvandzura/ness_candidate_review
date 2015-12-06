05512d49a713ee72f591d6e4b3c7f424abc2a821
Marián Boržik <sajajdo@outlook.com>
2015-12-06 12:43:41 +0100

    Design changes, Bug fixing

--- candidate_review/src/main/webapp/controllers/testsControlController.js
+++ candidate_review/src/main/webapp/controllers/testsControlController.js
@@ -1,7 +1,7 @@
 /**
  * Created by sajaj on 05.12.2015.
  */
-angular.module('NESS-TCFA').controller('testsControlController',['$scope','$rootScope','$http', function ($scope,$rootScope,$http) {
+angular.module('NESS-TCFA').controller('testsControlController',['$scope','$rootScope','$http','$window', function ($scope,$rootScope,$http,$window) {
   $scope.tests = [];
   $http.get($rootScope.serverUrl+'admin/mytests/'+$rootScope.settings.user.id+"?access_token="+$rootScope.settings.auth.access_token).then(
     function(response){
@@ -13,17 +13,24 @@ angular.module('NESS-TCFA').controller('testsControlController',['$scope','$root
       $rootScope.connProblem.problem = true;
     }
   );
+  $scope.deleting = false;
   $scope.removeTest = function(test){
-    $http.delete($rootScope.serverUrl+'test/'+test.id+"?access_token="+$rootScope.settings.auth.access_token).then(
+    $scope.deleting = true;
+    $http.delete($rootScope.serverUrl+'admin/test/'+test.id+"?access_token="+$rootScope.settings.auth.access_token).then(
       function(response){
+        $scope.deleting = false;
         $scope.tests.splice($scope.tests.indexOf(test),1);
       },
       function(){
-        console.log("Connecting problem!");
-        $rootScope.connProblem.problem = true;
+        $scope.deleting = false;
       }
     );
   };
+  $scope.$on('$locationChangeStart', function( event ) {
+     if ($scope.deleting == true) {
+       $window.location.href = '#/testsControl/';
+     }
+   });
   $scope.activeTests = function(){
     var number = 0;
     for(test in $scope.tests)
